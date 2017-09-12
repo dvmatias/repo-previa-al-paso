c6368d99c2d8a054b0d51492623d4c28f4f8f093
Matias <matias.delv.dom@gmail.com>
2017-09-12 20:02:51 -0300

    custom dialog design - first commit

---
+++ app/src/main/java/custom/BaseDialogFragment.java
@@ -0,0 +1,42 @@
+package custom;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by dvmatias on 11/09/17. Designed to keep hold of the instance of the Activity.
+ * So when the Dialog is attached to the Activity, you will know the instance of the
+ * Activity which created it.
+ */
+
+public abstract class BaseDialogFragment<T> extends DialogFragment {
+    /**
+     * Activity instance.
+     */
+    private T mActivityInstance;
+
+    /**
+     * Instance getter.
+     *
+     * @return - [T] Activity instance
+     */
+    public final T getActivityInstance() {
+        return mActivityInstance;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        mActivityInstance = (T) activity;
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mActivityInstance = null;
+    }
+
+}
